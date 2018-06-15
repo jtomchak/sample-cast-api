1864221d7612c9fa245477075ea959f879d2e766
Jesse Tomchak <jtomchak@gmail.com>
2018-06-15 15:25:06 -0700

    CORS addition

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 
 var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
