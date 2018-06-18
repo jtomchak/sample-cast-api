88b7c6c17d1179e6b9f1c9b25aef7d932902c92a
Jesse Tomchak <jtomchak@gmail.com>
2018-06-18 14:29:11 -0700

    more cors udpate

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.options("*", cors()); // include before other routes
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
