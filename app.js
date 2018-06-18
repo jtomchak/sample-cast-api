ceef3259ea6d4b56f191830ee742444ddc7ae142
Jesse Tomchak <jtomchak@gmail.com>
2018-06-18 14:36:34 -0700

    will it stick

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var indexRouter = require("./routes/index");
 
 var app = express();
 // app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
