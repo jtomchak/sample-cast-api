ace1b1b52b06d3421b77ee78a1c74fbae7f122ea
Jesse Tomchak <jtomchak@gmail.com>
2018-06-18 14:17:20 -0700

    cors update for PUT, ugh

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
+var cors = require("cors");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -7,14 +8,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 
 var app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -26,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.options("*", cors()); // include before other routes
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
