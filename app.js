f00974e11ee72b979a5cf96d8c191de43913ab79
Jesse Tomchak <jtomchak@gmail.com>
2018-06-18 14:40:23 -0700

    PATCH

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
   );
   next();
 });
